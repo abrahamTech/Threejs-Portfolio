c5e2dae0a56759f170f90d6779ab5d2c98e380f1
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-06-12 23:50:49 -0600

    Background and github icon from the work section's project cards have been included

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div
+      variants={fadeIn("up", "spring", index * 1, 0.75)}
+    >
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          {/* Cover Img */}
+          <img src={image} alt={name} className="w-full h-full object-cover rounded-xl"/>
+
+          {/* Github Img */}
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            onClick={() => window.open(source_code_link, "_blank")}
+            >
+              <img src={github} alt="github" className="w-2/3 h-2/3 object-contain"/>
+            </div>
+          </div>
+        </div>
+        {/* 1:55:50 */}
+      </Tilt>
+    </motion.div>
+  )
+};
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[1.9rem]"
+        >
+          Following projects showcases my skills and experience through real-world examples of my work. Each project is in my Gihub repository. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively. 
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key = {`project-${index}`}
+            index = {index}
+            {...project}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
